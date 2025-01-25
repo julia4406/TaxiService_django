fef345ae8387fadfe3eae9a127f66f77f2663f3a
Julia Alexeeva <julia4406@gmail.com>
2025-01-25 15:03:01 +0200

    Added new functionality as: driver_list, driver_details, car_list, car_detils, manufacturer_list (all as class-based views), paginatoin added to pages

--- taxi/views.py
+++ taxi/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from taxi.models import Car, Driver, Manufacturer
 
@@ -17,3 +18,29 @@ def index(request: HttpRequest) -> HttpResponse:
         "taxi/index.html",
         context=context
     )
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+
+
+class CarListView(ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer")
+    paginate_by = 5
+
+
+class CarDetailView(DetailView):
+    model = Car
+
+
+class DriverListView(ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars")
